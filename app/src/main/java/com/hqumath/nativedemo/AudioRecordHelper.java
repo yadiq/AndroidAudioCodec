af6acb157da50daab4556958be8c0c75624c5dfe
gyd <yadiq@qq.com>
2025-01-07 23:04:25 +0800

    添加音频编解码

---
+++ app/src/main/java/com/hqumath/nativedemo/AudioRecordHelper.java
@@ -0,0 +1,117 @@
+package com.hqumath.nativedemo;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
+import com.hqumath.nativedemo.app.AppExecutors;
+
+/**
+ * 手机拾音，录制和发送数据
+ */
+public class AudioRecordHelper {
+    public static final String TAG = "AudioRecordHelper";
+
+    private boolean isRunning = false;//音频录制线程状态
+    private int minBufferSize;//最小缓冲区大小
+    private byte[] receiveData;//每次收到的数据240B
+
+    private AudioRecord audioRec;//录音机
+    private AudioRecordListener listener;
+
+    public void init(AudioRecordListener listener) {
+        this.listener = listener;
+        initAudioRecord();
+    }
+
+    public void start() {
+        //未授予录音权限，无法播放
+        try {
+            if (audioRec != null)
+                audioRec.startRecording();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        if (!isRunning) {
+            isRunning = true;
+            AppExecutors.getInstance().workThread().execute(() -> {
+                //Log.d(TAG, "Record 线程开始");
+                while (isRunning) {
+                    //读取
+                    int reader = audioRec.read(receiveData, 0, receiveData.length);
+                    if (reader <= 0) continue;
+                    if (listener != null)
+                        listener.onAudioRecord(receiveData, reader);
+                }
+                //Log.d(TAG, "Record 线程结束");
+            });
+        }
+    }
+
+    public void stop() {
+        try {
+            if (audioRec != null)
+                audioRec.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        isRunning = false;
+    }
+
+    public void close() {
+        if (audioRec != null) {
+            audioRec.release();
+            audioRec = null;
+        }
+    }
+
+    //音频采集
+    private void initAudioRecord() {
+        //采样频率，每秒声音样本的次数。
+        int sampleRate = 8000;
+        //声道数，每个时刻有几份信息。单声道
+        int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+        //采样位数，将样本幅度量化，类似声卡的分辨率。
+        int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+        //最小缓冲区大小
+        minBufferSize = AudioRecord.getMinBufferSize(
+                sampleRate,
+                channelConfig,
+                audioFormat);
+        receiveData = new byte[minBufferSize];//每次收到的数据
+        //录音机
+        audioRec = new AudioRecord(
+                MediaRecorder.AudioSource.MIC,
+                sampleRate,
+                channelConfig,
+                audioFormat,
+                minBufferSize);
+        /*//声学回声消除器 AcousticEchoCanceler 消除了从远程捕捉到音频信号上的信号的作用
+        if (AcousticEchoCanceler.isAvailable()) {
+            AcousticEchoCanceler aec = AcousticEchoCanceler.create(audioRec.getAudioSessionId());
+            if (aec != null) {
+                aec.setEnabled(true);
+            }
+        }
+        //自动增益控制 AutomaticGainControl 自动恢复正常捕获的信号输出
+        if (AutomaticGainControl.isAvailable()) {
+            AutomaticGainControl agc = AutomaticGainControl.create(audioRec.getAudioSessionId());
+            if (agc != null) {
+                agc.setEnabled(true);
+            }
+        }
+        //噪声抑制器 NoiseSuppressor 可以消除被捕获信号的背景噪音
+        if (NoiseSuppressor.isAvailable()) {
+            NoiseSuppressor nc = NoiseSuppressor.create(audioRec.getAudioSessionId());
+            if (nc != null) {
+                nc.setEnabled(true);
+            }
+        }*/
+    }
+
+    public interface AudioRecordListener {
+        void onAudioRecord(byte[] data, int length);
+    }
+}
--- app/src/main/java/com/hqumath/nativedemo/MainActivity.java
+++ app/src/main/java/com/hqumath/nativedemo/MainActivity.java
@@ -1,8 +1,17 @@
 package com.hqumath.nativedemo;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.hqumath.nativedemo.databinding.ActivityMainBinding;
 import com.hqumath.nativedemo.utils.ByteUtil;
@@ -11,6 +20,9 @@ import com.hqumath.nativedemo.utils.LogUtil;
 
 import org.freedesktop.audiocodec.AudioCodec;
 import org.freedesktop.demo.Demo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * ****************************************************************
@@ -23,38 +35,122 @@ import org.freedesktop.demo.Demo;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private final String TAG = "Native";
+    private final int REQUEST_RECORD_AUDIO = 0x01;//请求录音权限
+    private Activity mContext;
+    private List<byte[]> audioData = new ArrayList<>();
+
+    private AudioRecordHelper audioRecordHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        mContext = this;
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         CommonUtil.init(this);
+        //事件监听
+        initListener();
+        //初始化数据
+        initData();
+    }
 
-        //网络请求
-        binding.btnHttp.setOnClickListener(v -> {
-            byte[] data1 = ByteUtil.hexToBytes("010203040506");
-            LogUtil.d(" data1=" + ByteUtil.bytesToHexWithSpace(data1));
-            byte[] data2 = new byte[data1.length];
-            int len2 = AudioCodec.g711Encode(data1, data2, data1.length, 1);
-            LogUtil.d("g711Encode: len=" + len2 + " data=" + ByteUtil.bytesToHexWithSpace(data2));
-            byte[] data3 = new byte[len2 * 2];
-            int len3 = AudioCodec.g711Decode(data2, data3, len2, 1);
-            LogUtil.d("g711Decode: len=" + len3 + " data=" + ByteUtil.bytesToHexWithSpace(data3));
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_RECORD_AUDIO) {
+            if (grantResults.length > 0) {
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    CommonUtil.toast("录音权限被您拒绝，请您到设置页面手动授权");
+                }
+            }
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void initListener() {
+        //录音并编码
+        binding.btnEncode.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        //检查录音权限
+                        if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+                            //清空缓存
+                            audioData.clear();
+                            //开始录音
+                            audioRecordHelper.start();
+                        } else {
+                            ActivityCompat.requestPermissions(mContext, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO);
+                        }
+                        break;
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+                            audioRecordHelper.stop();
+                        }
+                        break;
+                }
+                return true;
+            }
         });
+//        binding.btnEncode.setOnClickListener(v -> {
+//            //检查录音权限
+//            if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+//
+//
+//            } else {
+//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO);
+//            }
+//
+////            byte[] data1 = ByteUtil.hexToBytes("010203040506010203040506010203040506");
+////            LogUtil.d(" data1=" + ByteUtil.bytesToHexWithSpace(data1));
+////            byte[] data2 = new byte[data1.length];
+////            int len2 = AudioCodec.g711Encode(data1, data2, data1.length, 1);
+////            LogUtil.d("g711Encode: len=" + len2 + " data=" + ByteUtil.bytesToHexWithSpace(data2));
+////            byte[] data3 = new byte[len2 * 2];
+////            int len3 = AudioCodec.g711Decode(data2, data3, len2, 1);
+////            LogUtil.d("g711Decode: len=" + len3 + " data=" + ByteUtil.bytesToHexWithSpace(data3));
+//
+//        });
 
         //加解密
-        binding.btnEncrypt.setOnClickListener(v -> {
-            LogUtil.d("测试日志");
-//            String plaintext = "123abc";
-//            String key = "12345678901234567890123456789012";
-//            String iv = "1234567890123456";
-//            String result = Demo.encryptTest(plaintext, key, iv);
-//            binding.tv1.setText("aes256cbc加密:\n  明文: " + plaintext + "\n  密钥: " + key + "\n  偏移量: " + iv + "\n  结果: " + result + "\n更多加解密方式，见日志: \n  aes256gcm、aes256cbc、aes128ecb、\n  sha1、hmacSHA256");
-        });
+        binding.btnDecode.setOnClickListener(v -> {
+//            byte[] data = new byte[240];//每次收到的数据240B
+//            byte[] encodeData = new byte[240];//编码后的数据240B
+//            byte[] decodeData = new byte[1600];//解码后大小480B
+//
+//            int encodeLen = AudioCodec.g711Encode(data, encodeData, data.length, 1);
+//            int decodeLen = AudioCodec.g711Decode(data, decodeData, data.length, 1);
+//            binding.tvResult.setText("原始长度=" + data.length + "\n编码后长度=" + encodeLen + "\n解码后长度=" + decodeLen);
+//
+//            int encodeLen = AudioCodec.g723Encode(data, encodeData, data.length);
+//            int decodeLen = AudioCodec.g723Decode(data, decodeData, data.length);
+//            binding.tvResult.setText("原始长度=" + data.length + "\n编码后长度=" + encodeLen + "\n解码后长度=" + decodeLen);
 
+        });
     }
+
+    private void initData() {
+        audioRecordHelper = new AudioRecordHelper();
+        audioRecordHelper.init(new AudioRecordHelper.AudioRecordListener() {
+            @Override
+            public void onAudioRecord(byte[] data1, int len1) {
+                //byte[] data1 = ByteUtil.subByte(data, 0, length);
+                //编码
+                byte[] data2 = new byte[len1];
+                int len2 = AudioCodec.g711Encode(data1, data2, len1, 1);
+                data2 = ByteUtil.subByte(data2, 0, len2);
+                //存入列表
+                audioData.add(data2);
+            }
+        });
+    }
+
+
+
 }
